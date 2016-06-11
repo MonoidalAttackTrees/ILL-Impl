118f9757161bdb68f782565cbbc8085f5685279b
Matthew Tennis <mtennis@augusta.edu>
2016-06-11 18:27:31 -0400

    Building better test cases
    Commiting to switch  workspaces, updating T/L.

--- case-study/Testing.hs
+++ case-study/Testing.hs
@@ -4,22 +4,28 @@ module Testing where
 
 import Syntax
 import Parser
-import Pretty 
+import Pretty
+import TypeCheck 
 
-term1 = Fun (Arr Nat Nat) $ bind (s2n "y") Zero
-term2 = Var (s2n "x")
-term3 = App term1 term2
-term4 = Fun (Arr Nat Nat) (bind (s2n "y") $ App term1 term1)
+badterm1 = Fun (Arr Nat Nat) $ bind (s2n "y") Zero
+-- badterm2 = Fun (Arr Nat Nat) (bind (s2n "y") $ App term1 term1)
 
+term_y = Var (s2n "y")
+term1 = Fun Nat $ bind (s2n "x") term_y
+term2 = Fun (Arr Nat Nat) (bind (s2n "z") term1)
+term3 = Rec (Suc(Suc(Zero))) term2 term1
+
+ctx = extCtx emptyCtx nm ty 
+       where 
+        (nm,ty) = ((fst $ head $ parseCtx $ runPrettyTerm term1),
+                   (snd $ head $ parseCtx $ runPrettyTerm term1))
+	
 main = do
 	let a = runPrettyTerm term1
-	let b = runPrettyTerm term2
-	let c = runPrettyTerm term3
-	let d = runPrettyTerm $ App term1 term2
-	let e = runPrettyTerm $ App term1 term1
-	let f = runPrettyTerm term4
 	putStrLn a
+	let b = runPrettyTerm term2
+	putStrLn b
+	let c = runPrettyTerm term3
 	putStrLn c
-	putStrLn d
-	putStrLn e
-	putStrLn f
+
+	
